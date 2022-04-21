6a62c16aebc9bdcf9fa872cb3e8d82981b2155aa
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2022-04-21 20:37:37 +0200

    Shop

---
+++ VinterProjekt-2022/Shop.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using System;
+
+namespace VinterProjekt_2022
+{
+    public class Shop
+    {
+        Random random = new Random();
+
+        List<string> shopFood = new List<string>() { "Bread", "Cheesewheel", "Cabbage", "Sweetroll", "Cabbage Stew" };
+
+        List<string> shopPotion = new List<string>() { "Health Potion", "Stamina Potion", "Instant Death Potion" };
+
+        List<string> shopWeapon = new List<string>() { "Little Sword", "Glass Sword", "Cherry Mace", "Devil's Dagger" };
+
+
+    }
+}
